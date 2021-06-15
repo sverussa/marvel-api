2fa8a9108ef427b42c7e0127bd70b4ba1e487f3b
saulo <sauloverussa@gmail.,com>
2021-06-15 14:36:14 -0300

    Teste se a API está rodando monitorando spring boot actuator heath

--- src/test/java/api/marvel/MarvelApiApplicationTests.java
+++ src/test/java/api/marvel/MarvelApiApplicationTests.java
@@ -1,20 +1,52 @@
 package api.marvel;
 
 import api.marvel.service.CharacterService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.Assert;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@WebAppConfiguration
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class MarvelApiApplicationTests {
 
     @Autowired
     CharacterService characterService;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    private static final String CONTENT_TYPE = "application/json";
+
+    @BeforeEach
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
 
     @Test
     void contextLoads() {
         Assert.isTrue(true, "Ocorreu True");
     }
 
+    @Test
+    public void given_actuator_is_running_when_get_then_returns200() throws Exception {
+        mockMvc.perform(get("/actuator/health")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
 }
--- src/test/java/api/marvel/controller/CharacterControllerTest.java
+++ src/test/java/api/marvel/controller/CharacterControllerTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
