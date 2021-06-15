8cd4cc4f28b8565e638803bfb169d1aa94dd19e6
saulo <sauloverussa@gmail.,com>
2021-06-15 19:01:14 -0300

    Adicionando Summary no Character

--- src/main/java/api/marvel/service/impl/CharacterServiceImpl.java
+++ src/main/java/api/marvel/service/impl/CharacterServiceImpl.java
@@ -3,9 +3,11 @@ package api.marvel.service.impl;
 import api.marvel.entities.Character;
 import api.marvel.entities.container.CharacterDataContainer;
 import api.marvel.entities.list.ComicList;
+import api.marvel.entities.list.EventList;
+import api.marvel.entities.list.SeriesList;
+import api.marvel.entities.list.StoryList;
 import api.marvel.repository.CharacterRepository;
-import api.marvel.service.CharacterService;
-import api.marvel.service.ComicsService;
+import api.marvel.service.*;
 import api.marvel.validation.Parameters;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +23,24 @@ public class CharacterServiceImpl implements CharacterService {
     CharacterRepository characterRepository;
 
     ComicsService comicsService;
+    EventsService eventsService;
+    SeriesService seriesService;
+    StoriesService storiesService;
+
+    @Autowired
+    public void setEventsService(EventsService eventsService) {
+        this.eventsService = eventsService;
+    }
+
+    @Autowired
+    public void setSeriesService(SeriesService seriesService) {
+        this.seriesService = seriesService;
+    }
+
+    @Autowired
+    public void setStoriesService(StoriesService storiesService) {
+        this.storiesService = storiesService;
+    }
 
     @Autowired
     public void setCharacterRepository(CharacterRepository characterRepository) {
@@ -36,7 +56,13 @@ public class CharacterServiceImpl implements CharacterService {
         List<Character> characters = characterRepository.findAll();
         for (Character character : characters) {
             ComicList comics = comicsService.findSummaryByIdCharacter(character.getId());
+            EventList events = eventsService.findSummaryByIdCharacter(character.getId());
+            StoryList stories = storiesService.findSummaryByIdCharacter(character.getId());
+            SeriesList series = seriesService.findSummaryByIdCharacter(character.getId());
             character.setComics(comics);
+            character.setEvents(events);
+            character.setStories(stories);
+            character.setSeries(series);
         }
 
         CharacterDataContainer characterDataContainer = new CharacterDataContainer();
@@ -47,8 +73,15 @@ public class CharacterServiceImpl implements CharacterService {
     @Override
     public CharacterDataContainer findById(Integer id, Parameters parameters) {
         Character character = characterRepository.findById(id).orElse(null);
+
         ComicList comics = comicsService.findSummaryByIdCharacter(character.getId());
+        EventList events = eventsService.findSummaryByIdCharacter(character.getId());
+        StoryList stories = storiesService.findSummaryByIdCharacter(character.getId());
+        SeriesList series = seriesService.findSummaryByIdCharacter(character.getId());
         character.setComics(comics);
+        character.setEvents(events);
+        character.setStories(stories);
+        character.setSeries(series);
 
         CharacterDataContainer characterDataContainer = new CharacterDataContainer();
         characterDataContainer.setResults(Arrays.asList(character));
